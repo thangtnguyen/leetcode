04731d6a3ec48fb0b0e26dd97404b640d5ebd30a
Thang The Nguyen <thangnguyen0722@gmail.com>
2021-11-30 17:19:53 +0700

    Added TwoSum initially

---
+++ LeetCode/TwoSum/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            int i = 0;
+            while (i < nums.Length - 1 )
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+
+                        return result;
+                    }
+                }
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
