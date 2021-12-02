fd085d0a74356d2044754f6cdd0738c2e6eafea5
Thang The Nguyen <thangnguyen0722@gmail.com>
2021-12-02 15:06:40 +0700

    Added Longest Substring without repeating characters

---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LongestSubstringWithoutRepeatingCharacters;
+
+Solution sol = new Solution();
+var len = sol.LengthOfLongestSubstring("dvdf");
+
+Console.WriteLine(len);
---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+            else if (s.Length == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                Queue<char> queue = new Queue<char>();
+                int result = 0;
+
+                foreach (char c in s)
+                {
+                    if (queue.Contains(c))
+                    {
+                        while (queue.Contains(c))
+                        {
+                            queue.Dequeue();
+                        }
+                        queue.Enqueue(c);
+                    }
+                    else
+                    {
+                        queue.Enqueue(c);
+                    }
+
+                    if (result < queue.Count)
+                    {
+                        result = queue.Count;
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
