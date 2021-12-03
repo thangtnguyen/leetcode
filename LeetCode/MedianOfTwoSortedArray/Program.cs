b7ddfb9ec1e4e1b0263abaa682ec7601ef038ac5
Thang The Nguyen <thangnguyen0722@gmail.com>
2021-12-03 14:11:10 +0700

    Added Median Of Two Sorted Array

---
+++ LeetCode/MedianOfTwoSortedArray/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MedianOfTwoSortedArray;
+
+Solution sol = new Solution();
+var result = sol.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
+Console.WriteLine(result);
---
+++ LeetCode/MedianOfTwoSortedArray/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedianOfTwoSortedArray
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            List<int> merged = new List<int>();
+            
+            merged.AddRange(nums1);
+            merged.AddRange(nums2);
+            merged.Sort();
+
+            if (merged.Count == 0)
+            {
+                return 0;
+            }
+            else if (merged.Count == 1)
+            {
+                return merged[0];
+            }
+            else
+            {
+                int selectedIndex = merged.Count / 2;
+                if (merged.Count % 2 == 0)
+                {   
+                    return (double)(merged[selectedIndex - 1] + merged[selectedIndex]) / 2;
+                }
+                else
+                {   
+                    return (merged[selectedIndex]);
+                }
+            }
+        }
+    }
+}
