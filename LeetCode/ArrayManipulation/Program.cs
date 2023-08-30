0bdd6d563a810e1f2426d3526851e59ae5fa6b13
Thang Nguyen <Thang.Nguyen@codehq.nz>
2023-08-30 16:26:17 +0700

    Added ArrayManipulcation initially

---
+++ LeetCode/ArrayManipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ArrayManipulation;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 0; i < m; i++)
+        {
+            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+        }
+
+        long result = Result.arrayManipulation(n, queries);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+
+        //    textWriter.WriteLine(result);
+
+        //    textWriter.Flush();
+        //    textWriter.Close();
+        //}
+    }
+}
---
+++ LeetCode/ArrayManipulation/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulation
+{
+    public class Result
+    {
+        /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+        public static long arrayManipulation(int n, List<List<int>> queries)
+        {
+            long[] numList = new long[n];
+            
+            foreach (var query in queries)
+            {
+                for (int i = query[0] - 1; i < query[1]; i++)
+                {
+                    numList[i] += query[2];
+                }
+            }
+
+            return numList.Max();
+
+        }
+    }
+}
