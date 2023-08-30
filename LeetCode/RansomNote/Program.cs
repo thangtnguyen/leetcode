a55aa54fb0b2f90d48cc914088e9337f2cc88b1b
Thang Nguyen <Thang.Nguyen@codehq.nz>
2023-08-31 04:10:59 +0700

    Added RansomNote

---
+++ LeetCode/RansomNote/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RansomNote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int m = Convert.ToInt32(firstMultipleInput[0]);
+
+            int n = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<string> magazine = Console.ReadLine().TrimEnd().Split(' ').ToList();
+
+            List<string> note = Console.ReadLine().TrimEnd().Split(' ').ToList();
+
+            Result.checkMagazine(magazine, note);
+        }
+    }
+}
---
+++ LeetCode/RansomNote/Result.cs
@@ -0,0 +1,49 @@
+﻿namespace RansomNote
+{
+    internal class Result
+    {
+        /*
+     * Complete the 'checkMagazine' function below.
+     *
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY magazine
+     *  2. STRING_ARRAY note
+     */
+
+        public static void checkMagazine(List<string> magazine, List<string> note)
+        {
+            Dictionary<string, long> magazineWords = new Dictionary<string, long>();
+            foreach (string word in magazine)
+            {
+                if (magazineWords.ContainsKey(word))
+                {
+                    magazineWords[word] += 1;
+                }
+                else
+                {
+                    magazineWords[word] = 1;
+                }
+            }
+
+            foreach (string word in note)
+            {
+                if (magazineWords.ContainsKey(word))
+                {
+                    magazineWords[word] -= 1;
+                    if (magazineWords[word] < 0)
+                    {
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
+        }
+    }
+}
