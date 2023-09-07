32605a937ff4ca97c0448ca35d1ad2136fdd00b7
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-07 09:45:02 +0700

    Added ReverseInteger

---
+++ LeetCode/ReverseInteger/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReverseInteger;
+
+Console.WriteLine("Input value");
+var input = Console.ReadLine();
+var valueInput = Convert.ToInt32(input);
+Solution solution = new Solution();
+var result = solution.Reverse(valueInput);
+Console.WriteLine(result);
+
---
+++ LeetCode/ReverseInteger/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            var stringVal = new string(Convert.ToString(x).Reverse().ToArray());
+            if (x < 0)
+            {
+                stringVal = "-" + stringVal.Remove(stringVal.Length - 1, 1);
+            }
+            try
+            {
+                return Convert.ToInt32(stringVal);
+            }
+            catch (OverflowException ex)
+            {
+                return 0;
+            }
+        }
+        
+    }
+}
