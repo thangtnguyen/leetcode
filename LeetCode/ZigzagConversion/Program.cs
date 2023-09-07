fb0509ae5b42476d07f46014618f39c2db0a25fa
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-07 09:28:15 +0700

    Added ZigzagConversion

---
+++ LeetCode/ZigzagConversion/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ZigzagConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string: ");
+            var s = Console.ReadLine();
+            Console.WriteLine("Number of rows: ");
+            var numRows = Console.ReadLine();
+            Solution solution = new Solution();
+            var output = solution.Convert(s, Convert.ToInt32(numRows));
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ LeetCode/ZigzagConversion/Solution.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZigzagConversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            List<List<string>> list = new List<List<string>>();
+
+            int rowIndex = 0; // < numRows - 1
+            int columnIndex = 0; // < numRows
+            int group = 2 * (numRows - 1);
+            List<string> charList = new List<string>();
+
+            if (numRows > 1) 
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    int currentRowIndex = rowIndex % (numRows - 1);
+                    if (currentRowIndex == 0)
+                    {
+                        if (columnIndex == 0)
+                        {
+                            charList = new List<string>(numRows)
+                        {
+                            s[j].ToString()
+                        };
+                            ++columnIndex;
+                            if (j == s.Length - 1)
+                            {
+                                for (int k = columnIndex; k < numRows; k++)
+                                {
+                                    charList.Add(string.Empty);
+                                }
+                                list.Add(charList);
+                            }
+                        }
+                        else if (columnIndex == numRows - 1)
+                        {
+                            charList.Add(s[j].ToString());
+                            columnIndex = 0;
+                            ++rowIndex;
+                            list.Add(charList);
+                        }
+                        else
+                        {
+                            charList.Add(s[j].ToString());
+                            ++columnIndex;
+                            if (j == s.Length - 1)
+                            {
+                                for (int k = columnIndex; k < numRows; k++)
+                                {
+                                    charList.Add(string.Empty);
+                                }
+                                list.Add(charList);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        charList = new List<string>(numRows);
+                        for (int i = 0; i < numRows; i++)
+                        {
+                            if (i == numRows - 1 - (currentRowIndex))
+                            {
+                                charList.Add(s[j].ToString());
+                            }
+                            else
+                            {
+                                charList.Add(string.Empty);
+                            }
+                        }
+                        list.Add(charList);
+                        ++rowIndex;
+                    }
+                }
+
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < numRows; i++)
+                {
+                    foreach (var item in list)
+                    {
+                        result.Append(item[i]);
+                    }
+                }
+
+                return result.ToString();
+            }
+            else
+            {
+                return s;
+            }
+        }
+    }
+}
