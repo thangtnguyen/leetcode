514485422f0d02d835bee957b2a09f8515226f55
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-11 10:26:33 +0700

    Added Counting Bits

---
+++ LeetCode/CountingBits/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBits
+{
+    public class Solution
+    {
+        public int[] CountBits(int n)
+        {
+            List<int> resultList = new List<int>();
+            for (int i = 0; i <= n; i++)
+            {
+                resultList.Add(Convert.ToString(i, 2).ToCharArray().Count(c => c == '1'));
+            }
+
+            return resultList.ToArray();
+        }
+    }
+}
