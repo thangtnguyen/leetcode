83ada53c0577e1e356e59cfdad4909aa0bf0fb6b
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-12 13:55:46 +0700

    Added LongestCommonPrefix

---
+++ LeetCode/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LongestCommonPrefix
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder result = new StringBuilder();
+            //get the first item
+            string? firstItem = strs.FirstOrDefault();
+            if (!string.IsNullOrEmpty(firstItem))
+            {
+                
+                foreach (char c in firstItem)
+                {
+                    result.Append(c);
+                    if (!strs.All(p => p.StartsWith(result.ToString())))
+                    {
+                        result.Remove(result.Length - 1, 1);
+                        break;
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
