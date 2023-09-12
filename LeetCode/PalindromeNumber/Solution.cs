b9da476bb1c30c666402977113ed8405b761cb6d
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-12 11:51:56 +0700

    Added PalindromeNumber

---
+++ LeetCode/PalindromeNumber/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string stringVal = x.ToString();
+
+            return IsSymetric(stringVal);
+        }
+
+        private bool IsSymetric(string s)
+        {
+            return s.Equals(new string(s.Reverse().ToArray()));
+        }
+    }
+}
