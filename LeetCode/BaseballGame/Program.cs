bb0da432f3cd1f2fbe34d83549a9691341f87003
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-12 11:46:45 +0700

    Added BaseballGame

---
+++ LeetCode/BaseballGame/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BaseballGame;
+
+Console.WriteLine("Hello, World!");
+
+List<int> pointResult = new List<int>();
+
+Solution solution = new Solution();
+char[] space = new char[] { ' ' };
+
+string[] ops = Console.ReadLine().Split(space);
+int output = solution.CalPoints(ops);
+Console.WriteLine(output.ToString());
---
+++ LeetCode/BaseballGame/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballGame
+{
+    public class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            List<int> pointResult = new List<int>();
+
+            foreach (string op in ops)
+            {
+                int point;
+                if (int.TryParse(op, out point))
+                {
+                    pointResult.Add(point);
+                }
+                else if (op.Equals("C"))
+                {
+                    if (pointResult.Count > 0)
+                    {
+                        pointResult.RemoveRange(pointResult.Count - 1, 1);
+                    }
+                }
+                else if (op.Equals("D"))
+                {
+                    if (pointResult.Count > 0)
+                    {
+                        pointResult.Add(pointResult[pointResult.Count - 1] * 2);
+                    }
+                }
+                else
+                {
+                    if (pointResult.Count >= 2)
+                    {
+                        pointResult.Add(pointResult[pointResult.Count - 1] + pointResult[pointResult.Count - 2]);
+                    }
+                }
+            }
+
+            return pointResult.Sum();
+        }
+    }
+}
