8ac431156e987b8d6bd6906a5fc048b7c0135957
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-13 11:19:28 +0700

    Added ValidParentheses

---
+++ LeetCode/ValidParentheses/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+            {
+                s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            }
+
+            return s.Length == 0;
+        }
+    }
+}
