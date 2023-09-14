fe1501928ca9372413a746cefa6e45c29da2d976
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-14 11:40:41 +0700

    Added LengthOfLastWord

---
+++ LeetCode/LengthOfLastWord/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LengthOfLastWord
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            string endTrimString = s.TrimEnd();
+
+            int index = endTrimString.Length - 1;
+            int result = 0;
+            while (index >= 0 && endTrimString[index] != ' ')
+            {
+                index--;
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
