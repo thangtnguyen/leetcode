8aabe7cfa98c231ada7e2a21e254cd8b26a31a61
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-14 10:02:03 +0700

    Added RemoveDuplicatesFromSortedArray

---
+++ LeetCode/RemoveDuplidatesFromSortedArray/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RemoveDuplidatesFromSortedArray;
+
+Console.WriteLine("Hello, World!");
+
+int[] nums = new int[] { 1, 1, 2 };
+
+Solution solution = new Solution();
+solution.RemoveDuplicates(nums);
+foreach (int i in nums)
+{
+    Console.WriteLine(i);
+}
---
+++ LeetCode/RemoveDuplidatesFromSortedArray/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace RemoveDuplidatesFromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null) return 0;
+            if (nums.Length <= 1) return nums.Length;
+
+            int index = 0;
+            int lastValue = nums[index];
+            foreach (int x in nums)
+            {
+                if (x != lastValue)
+                {
+                    nums[++index] = x;
+                    lastValue = nums[index];
+                }
+            }
+
+            return index + 1;
+        }
+    }
+}
