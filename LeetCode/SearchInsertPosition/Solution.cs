1d8796aaca842d36ed9563664342237966c60fbf
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-14 11:29:15 +0700

    Added SearchInsertPosition

---
+++ LeetCode/SearchInsertPosition/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace SearchInsertPosition
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int lo = 0, hi = nums.Length;
+
+            while (lo < hi)
+            {
+                var mid = lo + (hi - lo) / 2;
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] < target) lo = mid + 1;
+                else hi = mid;
+            }
+
+            return lo;
+        }
+    }
+}
