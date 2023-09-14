bc91d9eada12258d32bd2202a7eeb73b853b43b5
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-14 10:30:47 +0700

    Added IndexOfTheFirstOccurenceInString

---
+++ LeetCode/IndexOfTheFirstOccurenceInString/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace IndexOfTheFirstOccurenceInString
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (!haystack.Contains(needle))
+            {
+                return -1;
+            }
+
+            return haystack.IndexOf(needle);
+        }
+    }
+}
