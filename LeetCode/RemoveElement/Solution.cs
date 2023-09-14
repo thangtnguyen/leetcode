64568d1cce80e2f443caa659540694dae2c7772f
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-14 10:22:14 +0700

    Added RemoveElement

---
+++ LeetCode/RemoveElement/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace RemoveElement
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums == null || nums.Length < 1) return 0;
+            if (nums.Length == 1)
+            {
+                if (nums[0] != val)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            int index = 0;
+
+            foreach (int n in nums)
+            {
+                if (n != val)
+                {
+                    nums[index++] = n;
+                }
+            }
+            return index;
+        }
+    }
+}
