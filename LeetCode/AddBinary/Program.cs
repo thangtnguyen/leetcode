9291e745ed45a12b26dda8b0dfe3845d63477b83
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-15 08:55:16 +0700

    Added AddBinary

---
+++ LeetCode/AddBinary/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddBinary;
+
+Console.WriteLine("Hello, World!");
+string a = "1010";// "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+string b = "1011"; // "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+Solution solution = new Solution();
+var result = solution.AddBinary(a, b);
+Console.Write(result);
---
+++ LeetCode/AddBinary/Solution.cs
@@ -0,0 +1,101 @@
+﻿namespace AddBinary
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            string result = string.Empty;
+            int aIndex = a.Length - 1;
+            int bIndex = b.Length - 1;
+            char carry = '0';
+
+            while (aIndex >= 0 && bIndex >=0)
+            {
+                var binResult = Add(a[aIndex], b[bIndex], carry);
+                if (binResult.Length == 2)
+                {
+                    result = result.Insert(0, binResult[1].ToString());
+                    carry = binResult[0];
+                }
+                else
+                {
+                    result = result.Insert(0, binResult);
+                    carry = '0';
+                }
+                --aIndex; 
+                --bIndex;
+            }
+
+            int p = -1;
+            string stringP = string.Empty;
+            if (aIndex >= 0)
+            {
+                p = aIndex;
+                stringP = a;
+            }
+            else if (bIndex >= 0)
+            {
+                p = bIndex;
+                stringP = b;
+            }
+            while (p >= 0)
+            {
+                var binResult = Add('0', stringP[p], carry);
+                if (binResult.Length == 2)
+                {
+                    result = result.Insert(0, binResult[1].ToString());
+                    carry = binResult[0];
+                }
+                else
+                {
+                    result = result.Insert(0, binResult);
+                    carry = '0';
+                }
+                --p;
+            }
+            if (carry == '1')
+            {
+                result = result.Insert(0, carry.ToString());
+            }
+
+            return result;
+        }
+
+        private string Add(char a,  char b, char carry = '0')
+        {
+            if (a == '0' && b == '0')
+            {
+                if (carry == '1')
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "0";
+                }
+            }
+            else if (a == '1' && b == '1')
+            {
+                if (carry == '1')
+                {
+                    return "11";
+                }
+                else
+                {
+                    return "10";
+                }
+            }
+            else
+            {
+                if (carry == '1')
+                {
+                    return "10";
+                }
+                else
+                {
+                    return "1";
+                }
+            }
+        }
+    }
+}
