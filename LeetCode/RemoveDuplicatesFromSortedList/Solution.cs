3c9f861657e5dc59197965ff3eb1602373917659
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-18 08:12:20 +0700

    Added RemoveDuplicatesFromSortedList

---
+++ LeetCode/RemoveDuplicatesFromSortedList/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoveDuplicatesFromSortedList
+{
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return null;
+
+            var p = head;            
+            
+            while (p.next != null)
+            {
+                if (p.val == p.next.val)
+                {
+                    p.next = p.next.next;
+                }
+                else
+                {
+                    p = p.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
