4f9f4374b47dd249df12073d49a3670b11c3d6ec
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-18 09:22:47 +0700

    Added ContainerWithMostWater

---
+++ LeetCode/ContainerWithMostWater/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContainerWithMostWater;
+
+Console.WriteLine("Hello, World!");
+
+int[] height = new int[]{ 2, 3, 4, 5, 18, 17, 6 };
+Solution solution= new Solution();
+solution.MaxArea(height);
---
+++ LeetCode/ContainerWithMostWater/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int leftIndex = 0, rightIndex = height.Length - 1;
+            int result = 0;
+            
+            while (rightIndex > leftIndex)
+            {
+                int area = Math.Min(height[rightIndex], height[leftIndex]) * (rightIndex - leftIndex);
+                result = Math.Max(result, area);
+
+                if (height[leftIndex] <= height[rightIndex]) 
+                {
+                    int temp = height[leftIndex];
+                    do
+                        leftIndex++;
+                    while (leftIndex < rightIndex && height[leftIndex] <= temp);
+                }
+                else
+                {
+                    int temp = height[rightIndex];
+                    do
+                        rightIndex--;
+                    while (leftIndex < rightIndex && height[rightIndex] <= temp);
+                }
+            }
+
+            return result;
+        }
+    }
+}
