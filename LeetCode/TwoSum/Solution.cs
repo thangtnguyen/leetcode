6be46d75aba024b98147f850f2e50c6fd8a84dc7
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-19 10:52:18 +0700

    Updated TwoSum

--- LeetCode/TwoSum/Solution.cs
+++ LeetCode/TwoSum/Solution.cs
@@ -1,30 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwoSum
+﻿namespace TwoSum
 {
     public class Solution
     {
         public int[] TwoSum(int[] nums, int target)
         {
-            int[] result = new int[2];
-            int i = 0;
-            while (i < nums.Length - 1 )
-            {
-                for (int j = i + 1; j < nums.Length; j++)
-                {
-                    if (nums[i] + nums[j] == target)
-                    {
-                        result[0] = i;
-                        result[1] = j;
+            //int[] result = new int[2];
+            //int i = 0;
+            //while (i < nums.Length - 1 )
+            //{
+            //    for (int j = i + 1; j < nums.Length; j++)
+            //    {
+            //        if (nums[i] + nums[j] == target)
+            //        {
+            //            result[0] = i;
+            //            result[1] = j;
 
-                        return result;
-                    }
+            //            return result;
+            //        }
+            //    }
+            //    i++;
+            //}
+            
+            //return result;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+
+                if (!map.ContainsKey(diff))
+                {
+                    map[nums[i]] = i;
                 }
-                i++;
+                else
+                {
+                    result[0] = i;
+                    result[1] = map[diff];
+                    break;
+                }
             }
 
             return result;
