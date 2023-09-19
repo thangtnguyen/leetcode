01eb91357468a2ba6e33f4e92c521d45a4e9ae1d
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-19 13:54:05 +0700

    Added BestTimeToBuyAndSellStock and ContainsDuplicate

---
+++ LeetCode/BestTimeToBuyAndSellStock/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace BestTimeToBuyAndSellStock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 1) return 0;
+
+            int left = 0;
+            int profit = 0;
+
+            for (int right = 1; right < prices.Length; right++)
+            {
+                if (prices[right] > prices[left])
+                {
+                    profit = Math.Max(profit, prices[right] - prices[left]);
+                }
+                else
+                {
+                    left = right;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ LeetCode/ContainsDuplicate/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace ContainsDuplicate
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    set.Add(nums[i]);
+                }
+            }
+
+            return false;
+        }
+    }
+}
