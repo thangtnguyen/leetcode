f110b73f413169fa6573e5b620366eb7d5fe2e89
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-20 08:57:34 +0700

    Added MaximumSubarray.

---
+++ LeetCode/MaximumSubarray/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace MaximumSubarray
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int currentSubMax = nums[0];
+            int maxSum = nums[0];
+            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentSubMax = Math.Max(currentSubMax + nums[i], nums[i]);
+                maxSum = Math.Max(maxSum, currentSubMax);
+            }
+
+            return maxSum;
+        }
+    }
+}
