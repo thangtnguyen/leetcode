42af10906c046c5a961a0871fdf386b580b81037
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-20 10:01:31 +0700

    Added MaximumProductSubarray

---
+++ LeetCode/MaximumProductSubarray/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MaximumProductSubarray;
+
+Console.WriteLine("Hello, World!");
+
+int[] nums = new int[] { 2, 3, -2, 4 };
+Solution solution = new Solution();
+var result = solution.MaxProduct(nums);
---
+++ LeetCode/MaximumProductSubarray/Solution.cs
@@ -0,0 +1,44 @@
+﻿namespace MaximumProductSubarray
+{
+    public class Solution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int min = 1, max = 1, result = max;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    min = 1;
+                    max = 1;
+                    if (i == 0)
+                    {
+                        result = nums[i];
+                    }
+                    else
+                    {
+                        result = Math.Max(result, nums[i]);
+                    }
+                }
+                else
+                {
+                    int tempMin = Math.Min(min * nums[i], max * nums[i]);
+                    int tempMax = Math.Max(min * nums[i], max * nums[i]);
+                    min = Math.Min(nums[i], tempMin);
+                    max = Math.Max(nums[i], tempMax);
+                    if (i == 0)
+                    {
+                        result = max;
+                    }
+                    else
+                    {
+                        result = Math.Max(max, result);
+                    }
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
