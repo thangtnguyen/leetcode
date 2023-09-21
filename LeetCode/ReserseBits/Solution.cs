8986be84cad48d89d666198290d20e4af2666580
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-21 14:29:41 +0700

    Added ReserveBits

---
+++ LeetCode/ReserseBits/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace ReserseBits
+{
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            uint reserve = 0;
+            for (int i = 1; i <= 32; i++)
+            {
+                reserve = reserve << 1;
+                reserve = reserve | (1 & n);
+                n = n >> 1;
+            }
+
+            return reserve;
+        }
+    }
+}
