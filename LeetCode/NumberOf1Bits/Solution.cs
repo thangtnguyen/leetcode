88395df62354262bae009f9c954b474511ca21a4
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-21 11:49:43 +0700

    Added NumberOf1Bits

---
+++ LeetCode/NumberOf1Bits/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace NumberOf1Bits
+{
+    public class Solution
+    {
+        public int HammingWeight(uint n)
+        {
+            uint result = 0;
+
+            while (n != 0)
+            {
+                result += n & 1;
+                n = n >> 1;
+            }
+
+            return (int)result;
+        }
+    }
+}
