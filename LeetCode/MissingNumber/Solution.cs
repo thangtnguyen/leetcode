1ccbeb740c11ab0e779ea74f0492dab7fe9b80eb
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-21 14:16:01 +0700

    Added MissingNumber

---
+++ LeetCode/MissingNumber/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace MissingNumber
+{
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int xor = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                xor = xor ^ nums[i];
+            }
+
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                xor = xor ^ i;
+            }
+
+            return xor;
+        }
+    }
+}
