d622e1491c5439c529c9dd6fc5dc86db162bc25a
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-22 11:03:27 +0700

    Added LongestIncreasingSubsequence

---
+++ LeetCode/LongestIncreasingSubsequence/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LongestIncreasingSubsequence
+{
+    public class Solution
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1) return 1;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            int result = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int len = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i]) 
+                    {
+                        len = Math.Max(len, dp[j]);
+                    }
+                }
+                dp[i] = len + 1;
+                result = Math.Max(result, dp[i]);
+            }
+
+            return result;
+        }
+    }
+}
