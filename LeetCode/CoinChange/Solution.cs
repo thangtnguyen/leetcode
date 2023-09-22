60f14b6c60971ce85df3b31836271c6bcd520be1
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-22 10:09:11 +0700

    Added CoinChange

---
+++ LeetCode/CoinChange/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace CoinChange
+{
+    public class Solution
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            int[] dp = new int[amount + 1];
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++) 
+            {
+                int min = int.MaxValue;
+                for (int j = 0; j < coins.Length; j++)
+                {
+                    if (coins[j] <= i && dp[i - coins[j]] != -1)
+                    {
+                        min = Math.Min(min, dp[i - coins[j]]);
+                    }
+                }
+                dp[i] = (min == int.MaxValue) ? -1 : min + 1;
+            }
+
+            return dp[amount];
+        }
+    }
+}
