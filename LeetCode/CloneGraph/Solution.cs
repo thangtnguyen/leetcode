5ca5ddacd7d65dd933bd011e69fe77edcf2cd054
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-22 11:56:00 +0700

    Added CloneGraph

---
+++ LeetCode/CloneGraph/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace CloneGraph
+{
+    public class Solution
+    {
+        public Node CloneGraph(Node node)
+        {
+            if (node == null) return null;
+
+            Dictionary<Node, Node> cloneMapping = new Dictionary<Node, Node>();
+            Queue<Node> handledNodes = new Queue<Node>();
+            Node newNode = new Node(node.val);
+            cloneMapping.Add(node, newNode);
+            handledNodes.Enqueue(node);
+            while(handledNodes.Any())
+            {
+                Node currentNode = handledNodes.Dequeue();
+                var newNeighbors = cloneMapping[currentNode].neighbors;
+                foreach (var neighbor in currentNode.neighbors)
+                {
+                    if (!cloneMapping.ContainsKey(neighbor))
+                    {
+                        Node tempNode = new Node(neighbor.val);
+                        cloneMapping.Add(neighbor, tempNode);
+                        handledNodes.Enqueue(neighbor);
+                    }
+                    newNeighbors.Add(cloneMapping.GetValueOrDefault(neighbor));
+                }
+            }
+
+            return newNode;
+        }
+    }
+}
