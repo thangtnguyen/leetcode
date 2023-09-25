832069ec7e635e56b3189091bbbfd4e1031730d7
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-25 08:39:32 +0700

    Added CourseSchedule

---
+++ LeetCode/CourseSchedule/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CourseSchedule;
+
+Console.WriteLine("Hello, World!");
+int numCourses = 2;
+int[][] prerequisites = new int[1][];
+prerequisites[0] = new int[]{ 1, 0};
+Solution solution = new Solution();
+var result = solution.CanFinish(numCourses, prerequisites);
+Console.WriteLine(result);
---
+++ LeetCode/CourseSchedule/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection.Metadata;
+
+namespace CourseSchedule
+{
+    public class Solution
+    {
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            if (numCourses <= 0) return false;
+            if (numCourses == 1) return true;
+
+            Dictionary<int, List<int>> prerequisiteNodes = new Dictionary<int, List<int>>();
+            int[] inDegree = new int[numCourses];
+            Queue<int> handlingQueue = new Queue<int>();
+
+            for (int i = 0; i < numCourses; i++)
+            {
+                prerequisiteNodes[i] = new List<int>();
+            }
+
+            foreach(var prerequisite in prerequisites)
+            {
+                prerequisiteNodes[prerequisite[1]].Add(prerequisite[0]);
+
+                inDegree[prerequisite[0]] += 1;
+            }
+
+            for (int i = 0; i < inDegree.Length; i++)
+            {
+                if (inDegree[i] == 0)
+                {
+                    handlingQueue.Enqueue(i);
+                }
+            }
+
+            if (!handlingQueue.Any()) return false;
+            int enrolled = 0;
+            while (handlingQueue.Any())
+            {
+                int enrolledCourse = handlingQueue.Dequeue();
+                enrolled += 1;
+                foreach (var node in prerequisiteNodes[enrolledCourse])
+                {
+                    if (inDegree[node] > 0)
+                    {
+                        inDegree[node] -= 1;
+                    }
+                    if (inDegree[node] == 0) handlingQueue.Enqueue(node);
+                }
+            }
+
+            return enrolled == numCourses;
+        }
+    }
+}
