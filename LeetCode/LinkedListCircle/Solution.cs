7f37296532516ad90b0e8f52d2d7ccea9d69cd59
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-25 14:43:23 +0700

    Added LinkedListCycle

---
+++ LeetCode/LinkedListCircle/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedListCircle
+{
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slowPointer = head, fastPointer = head;
+            while (slowPointer != null && fastPointer != null && fastPointer.next != null)
+            {
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next.next;
+                if (slowPointer == fastPointer) return true;
+            }
+
+            return false;
+        }
+    }
+}
