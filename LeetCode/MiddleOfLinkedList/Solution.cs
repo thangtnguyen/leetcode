b1e8aacf69ac31cccf40d11a80afec574c7e6e92
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-25 08:50:53 +0700

    Added MiddleOfLinkedList

---
+++ LeetCode/MiddleOfLinkedList/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace MiddleOfLinkedList
+{
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode slowPointer = head, fastPointer = head;
+
+            while (slowPointer != null && fastPointer != null && fastPointer.next != null) {
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next.next;
+            }
+
+            return slowPointer;
+        }
+    }
+}
