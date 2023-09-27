83fb88b9b7969f79735b2ad1dc084f74e27acf82
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-27 14:43:13 +0700

    Added InvertBinaryTree

---
+++ LeetCode/InvertBinaryTree/Solution.cs
@@ -0,0 +1,24 @@
+﻿using CommonDataStructures;
+
+namespace InvertBinaryTree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/invert-binary-tree/
+    /// </summary>
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+
+            TreeNode temp = root.Left;
+            root.Left = root.Right;
+            root.Right = temp;
+
+            InvertTree(root.Left);
+            InvertTree(root.Right);
+
+            return root;
+        }
+    }
+}
