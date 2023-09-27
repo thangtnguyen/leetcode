0c2c885bde4bd1192025a21f02e2f4b6d969192d
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-27 14:39:13 +0700

    Added common data structure and symmetrictree

---
+++ LeetCode/CommonDataStructures/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿namespace CommonDataStructures
+{
+    public class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) 
+        {
+            Val = val;
+            Left = left;
+            Right = right;
+        }
+    }
+}
---
+++ LeetCode/SymmetricTree/Solution.cs
@@ -0,0 +1,25 @@
+﻿using CommonDataStructures;
+
+namespace SymmetricTree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/symmetric-tree/
+    /// </summary>
+    public class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            return CheckMirror(root.Left, root.Right);
+        }
+
+        private bool CheckMirror(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null) return true;
+            if (left == null || right == null || left.Val != right.Val) return false;
+
+            return CheckMirror(left.Left, right.Right) && CheckMirror(left.Right, right.Left);
+        }
+    }
+}
