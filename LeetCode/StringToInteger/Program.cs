6ef380c6f7bc4c4333aee1bde2fea9125e2ee426
Thang Nguyen <thangnguyen0722@gmail.com>
2024-11-06 08:39:44 +0700

    Added StringToInteger

---
+++ LeetCode/StringToInteger/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringToInteger;
+
+Console.WriteLine("8. String to Integer");
+
+Solution solution = new Solution();
+int result = solution.MyAtoi("20000000000000000000");
+Console.WriteLine(result);
+Console.ReadLine();
---
+++ LeetCode/StringToInteger/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace StringToInteger
+{
+    public class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            string trimmedValue = s.Trim();
+
+            if (trimmedValue.Length == 0) return 0;
+
+            bool isNegattive = trimmedValue[0] == '-';
+                        
+            Stack<int> stack = new Stack<int>();
+            int i = 0;
+            while (i < trimmedValue.Length)
+            {
+                if (i == 0 && (trimmedValue[i] == '-' || trimmedValue[i] == '+'))
+                {
+                    i++;
+                    continue;
+                }
+
+                if (char.IsDigit(trimmedValue[i]))
+                {
+                    int value = Convert.ToInt32(trimmedValue[i].ToString());
+                    if (value != 0 || stack.Count != 0)
+                    {
+                        stack.Push(value);                        
+                    }
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            long temp = 0;
+            int numberOfDigits = stack.Count;
+            for (int j = 0; j < numberOfDigits; j++)
+            {
+                temp += (long)(stack.Pop() * Math.Pow(10, j));
+                if (temp > int.MaxValue)
+                {
+                    temp = int.MaxValue;
+                    break;
+                }
+            }
+
+            temp = (isNegattive ? -temp : temp);
+            temp = (temp < int.MinValue ? int.MinValue : temp);
+
+            return (int)temp;
+        }
+    }
+}
