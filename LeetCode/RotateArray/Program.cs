f8addde1da349dfb59a6b423d8bf3c024798a1c5
Thang Nguyen <thangnguyen0722@gmail.com>
2024-11-06 09:10:53 +0700

    Added RotateArray

---
+++ LeetCode/RotateArray/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RotateArray;
+
+Console.WriteLine("Hello, World!");
+
+int[] input = new int[] {1, 2,3 ,4, 5, 6, 7};
+Solution solution = new Solution();
+solution.Rotate(input, 3);
+
+for (int i = 0; i < input.Length; i++)
+{
+    Console.WriteLine(input[i]);
+}
---
+++ LeetCode/RotateArray/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace RotateArray
+{
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+
+            if (nums != null && nums.Length > 1)
+            {
+                if (nums.Length > k)
+                {
+                    int[] firstGroup = new int[nums.Length - k];
+                    int[] secondGroup = new int[k];
+
+
+                    for (int i = nums.Length - k, index = 0; i < nums.Length; i++, index++)
+                    {
+                        secondGroup[index] = nums[i];
+                    }
+
+                    for (int i = 0; i < nums.Length - k; i++)
+                    {
+                        firstGroup[i] = nums[i];
+                    }
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (i < k)
+                        {
+                            nums[i] = secondGroup[i];
+                        }
+                        else
+                        {
+                            nums[i] = firstGroup[i - k];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < k; i++)
+                    {
+                        int lastVal = nums[nums.Length - 1];
+                        int prevTemp = nums[0];
+
+                        for (int j = 1; j < nums.Length; j++)
+                        {
+                            int currTemp = nums[j];
+                            nums[j] = prevTemp;
+                            prevTemp = currTemp;
+                        }
+                        nums[0] = lastVal;
+                    }
+                }
+            }
+        }
+    }
+}
